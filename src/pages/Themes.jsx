ed113cd1fcadff40b104737b8fe799e4b6080969
Thiago Xavier <thiagox58@gmail.com>
2024-09-14 16:48:21 -0300

    feat: add error page in theme

--- src/pages/Themes.jsx
+++ src/pages/Themes.jsx
@@ -1,24 +1,43 @@
 import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { fetchQuizData } from '../services/quizApi';
 
 import '../styles/Theme.css'
 
 const Themes = () => {
   const [quizData, setQuizData] = useState({});
+  const [error, setError] = useState(null);
 
   const navigate = useNavigate();
 
   useEffect(() => {
     async function loadQuizData() {
-      const data = await fetchQuizData();
-      setQuizData(data)
+      try {
+        const data = await fetchQuizData();
+        setQuizData(data)
+      } catch (error) {
+        console.log(error)
+        setError('Algo deu errado!')
+      }
+
+
     }
     loadQuizData();
   }, []);
 
   const handleButton = (theme) => {
     navigate('/game', { state: { theme } });
+  }
+
+  if (error) {
+    return (
+      <>
+        <h1>{error}</h1>
+        <Link to="/">
+          <button>Voltar</button>
+        </Link>
+      </>
+    );
   }
 
   return (
